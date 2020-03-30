88f670cad9be050e83f6d69433465b1cfd05eafd
amazingokc <x408188680@gmail.com>
2020-03-30 10:55:20 +0800

    自定义view path操作
    Signed-off-by: 408188680@qq.com <xiao1375055954>

---
+++ app/src/main/java/com/example/testdemo/customView/CustomVeiw7.java
@@ -0,0 +1,88 @@
+package com.example.testdemo.customView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.testdemo.LLog;
+
+public class CustomVeiw7 extends View {
+    public CustomVeiw7(Context context) {
+        super(context);
+    }
+
+    public CustomVeiw7(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomVeiw7(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private Paint kuangPaint;
+
+    private void init() {
+        kuangPaint = new Paint();
+        kuangPaint.setAntiAlias(true);
+        kuangPaint.setColor(Color.BLACK);
+        kuangPaint.setStyle(Paint.Style.STROKE);
+        kuangPaint.setStrokeWidth(3f);
+
+    }
+
+    private int width;
+    private int height;
+    private int valueTypeCount = 6;
+    private int bigRadius;
+    private float angle = (float) (2 * Math.PI / valueTypeCount);
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = w;
+        height = h;
+        bigRadius = width / 2;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.translate(width / 2, height / 2);
+        drawKuang(canvas);
+    }
+
+    private void drawKuang(Canvas canvas) {
+        LLog.d("sasasa", 2 * Math.cos(60));
+        Path path = new Path();
+        int radius;
+
+        for (int i = 1; i <= 5; i++) {
+            radius = i * bigRadius / 5;
+            path.reset();
+
+            for (int j = 0; j < valueTypeCount; j++) {
+                if (j == 0) {
+                    path.moveTo(radius, 0);
+                } else {
+                    float x = (float) (radius * Math.cos(angle * j));
+                    float y = (float) (radius * Math.sin(angle * j));
+
+                    path.lineTo(x, y);
+                }
+            }
+            path.close();
+            canvas.drawPath(path, kuangPaint);
+
+        }
+
+    }
+
+}
