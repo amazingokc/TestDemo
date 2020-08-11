5370bf26069c49bc9eb816e10ad0702614ab2737
xiaoguoqing <408188680@qq.com>
2020-08-11 16:15:43 +0800

    hilt
    Signed-off-by: xiaoguoqing <408188680@qq.com>

---
+++ app/src/main/java/com/example/testdemo/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example.testdemo;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+/**
+ * Author:xiaoguoqing
+ * Time:2020/8/11   9:50
+ * Des:
+ */
+@HiltAndroidApp
+public class MyApplication extends Application {
+}
---
+++ app/src/main/java/com/example/testdemo/hilt/HiltExampleActivity.kt
@@ -0,0 +1,15 @@
+package com.example.testdemo.hilt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.test.R
+import dagger.hilt.android.AndroidEntryPoint
+
+//提供依赖项
+@AndroidEntryPoint
+class HiltExampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hilt_example)
+    }
+}
